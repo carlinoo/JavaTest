1701087a4ec8c94e9197092bfb88857440e11f4d
Carlino <carlinoo@icloud.com>
2018-02-14 17:34:49 +0000

    all works

--- ie/dit/Main.java
+++ ie/dit/Main.java
@@ -5,8 +5,10 @@ public class Main {
   public static void main(String[] args) {
     // Get the full path and pass it as a parameter
     TuneBook tb = new TuneBook(System.getProperty("user.dir") + "/file.abc");
-    // System.out.println(tb);
-    // System.out.println(tb.findTune("Hello World"));
-    tb.findTune("A").play();
+
+    System.out.println(tb);
+
+    Tune t = tb.findTune("Scotsman over the Border");
+    t.play();
   }
 }
