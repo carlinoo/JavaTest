32bf5fa42afd0df7fc02cb0812818cfaa21926e0
Carlino <carlinoo@icloud.com>
2018-02-14 16:25:50 +0000

    added TuneBook

--- ie/dit/Main.java
+++ ie/dit/Main.java
@@ -3,7 +3,8 @@ package ie.dit;
 
 public class Main {
   public static void main(String[] args) {
-    Tune t = new Tune();
+    Tune t = new Tune(2, "title", "altTitle", "notation");
     System.out.println(t);
+    TuneBook tb = new TuneBook("Hello String");
   }
 }
---
+++ ie/dit/TuneBook.java
@@ -0,0 +1,12 @@
+package ie.dit;
+import java.util.ArrayList;
+
+public class TuneBook {
+  private ArrayList<Tune> tunes;
+
+  TuneBook(String s) {
+    System.out.println(s);
+  }
+
+
+}
