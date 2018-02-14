b0739e0f31a2a496cd7b42d38a8c8afb062e2935
Carlino <carlinoo@icloud.com>
2018-02-14 16:50:48 +0000

    read from input file

--- ie/dit/Main.java
+++ ie/dit/Main.java
@@ -4,7 +4,8 @@ package ie.dit;
 public class Main {
   public static void main(String[] args) {
     Tune t = new Tune(2, "title", "altTitle", "notation");
-    System.out.println(t);
-    TuneBook tb = new TuneBook("Hello String");
+    // System.out.println(t);
+    // Get the full path and pass it as a parameter
+    TuneBook tb = new TuneBook(System.getProperty("user.dir") + "/" + args[0]);
   }
 }
--- ie/dit/TuneBook.java
+++ ie/dit/TuneBook.java
@@ -1,11 +1,49 @@
 package ie.dit;
 import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class TuneBook {
-  private ArrayList<Tune> tunes;
+  private ArrayList<Tune> tunes = new ArrayList<Tune>;
 
+
+  // Constructor
   TuneBook(String s) {
-    System.out.println(s);
+    BufferedReader inputStream = null;
+
+    // Try to read from bufeer
+    try {
+        inputStream = new BufferedReader(new FileReader(s));
+
+        String l;
+
+        while ((l = inputStream.readLine()) != null) {
+
+          // If there is an empty line, there is a new tume
+          if (l.trim().isEmpty()) {
+            System.out.println("LINEEEEE");
+          }
+          System.out.println(l);
+        }
+
+
+    } catch (IOException e) {
+        e.printStackTrace();
+
+
+
+    } finally {
+        if (inputStream != null) {
+            try {
+                inputStream.close();
+
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
   }
 
 
